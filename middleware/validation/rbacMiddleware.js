045820d25d9bdb75fba76a90cd1615745f0854f3
unknown <ibrahimallie905@gmail.com>
2024-05-27 23:12:52 +0300

    Added roles & permissions

---
+++ middleware/validation/rbacMiddleware.js
@@ -0,0 +1,16 @@
+const Role = require('../../models/role');
+const Permissions = require('../../models/permissions');
+
+// Check if the user has the required permission for a route
+exports.checkPermission = (permission) => {
+  return (req, res, next) => {
+    const userRole = req.user ? req.user.role : 'anonymous';
+    const userPermissions = new Permissions().getPermissionsByRoleName(userRole);
+
+    if (userPermissions.includes(permission)) {
+      return next();
+    } else {
+      return res.status(403).json({ error: 'Access denied' });
+    }
+  };
+};
