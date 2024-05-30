7a3993ab01a5c02a6203ef62880ad9d861c84c40
unknown <ibrahimallie905@gmail.com>
2024-05-30 15:02:16 +0300

    Work in progress post method create-booking

---
+++ models/bookingModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  bookingDate: { type: Date, default: Date.now },
+  duration: { type: Number, required: true }, // Duration in hours
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
